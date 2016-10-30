40325ae8487ce61cc59e64d2dbc6abb0abd9420b
stop2stare <emitremmus0235@163.com>
2016-10-30 15:49:36 +0800

    构建工具里增加autoprefixer与uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,54 @@
 var gulp = require('gulp'),
-    less = require('gulp-less'),
-    sourcemap = require('gulp-sourcemaps'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload;
+  less = require('gulp-less'),
+  sourcemap = require('gulp-sourcemaps'),
+  uglify = require('gulp-uglify'),
+  pump = require('pump'),
+  autoprefixer = require('gulp-autoprefixer'),
+  concat = require('gulp-concat'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload;
 
 var src = {
-    less: 'app/less/*.less',
-    css: 'app/css',
-    html: 'app/*.html',
-    map: 'app/maps'
+  less: 'app/less/*.less',
+  js: './app/js/*.js',
+  css: 'app/css',
+  html: 'app/*.html'
 };
 
 // Static Server + watching less/html files
 gulp.task('serve', ['less'], function() {
 
-    browserSync({
-        server: "./app"
-    });
+  browserSync({
+    server: "./app"
+  });
 
-    gulp.watch(src.less, ['less']);
-    gulp.watch(src.html).on('change', reload);
+  gulp.watch(src.less, ['less']);
+  gulp.watch(src.html).on('change', reload);
 });
 
 // Compile less into CSS
 gulp.task('less', function() {
-    return gulp.src(src.less)
-        .pipe(sourcemap.init())
-        .pipe(less())
-        .pipe(sourcemap.write(src.map))
-        .pipe(gulp.dest(src.css))
-        .pipe(reload({
-            stream: true
-        }));
+  return gulp.src(src.less)
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(concat('slide.css'))
+    .pipe(sourcemap.write())
+    .pipe(gulp.dest(src.css))
+    .pipe(reload({
+      stream: true
+    }));
+});
+
+// 压缩JS文件
+gulp.task('compress', function(cb) {
+  pump([
+      gulp.src(src.js),
+      uglify(),
+      gulp.dest('./dist')
+    ],
+    cb
+  );
 });
 
 gulp.task('default', ['serve']);
